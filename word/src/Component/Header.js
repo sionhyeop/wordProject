b422b6d868424614eb2d4d362c83addb185df43c
qbnyp <moneycloudy@naver.com>
2024-10-05 19:27:19 +0900

    feature : 헤더 작성

--- word/src/Component/Header.js
+++ word/src/Component/Header.js
@@ -1,11 +1,37 @@
 import s from "../Component/Header.module.scss"
+import logo from "../Component/Frame163.svg"
+import twitImg from "../Component/Social Icons.svg"
+import facebk from "../Component/instar.svg";
+import instar from "../Component/facebook.svg";
 
 export default function Header() {
+    
     return (
         <>
-            안녕
-
-            <div className={s.hi}>안녕</div>
+            <main className={s.container}>
+                <header className={s.headerContainer}>
+                    <nav className={s.nav}>
+                        <div className={s.plan}>기획</div>
+                        <div className={s.problem}>문제풀기</div>
+                        <div className={s.search}>탐색하기</div>
+                        <div className={s.intro}>소개</div>
+                    </nav>
+                    <div className={s.logoDiv}>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className={s.share}>
+                        <div className={s.imgDiv}>
+                            <img src={twitImg} alt="" srcset="" />
+                        </div>
+                        <div className={s.imgDiv}>
+                            <img src={instar} alt="" srcset="" />
+                        </div>
+                        <div className={s.imgDiv}>
+                            <img src={facebk} alt="" srcset="" />
+                        </div>
+                    </div>
+                </header>
+            </main>
         </>
     )
 }
