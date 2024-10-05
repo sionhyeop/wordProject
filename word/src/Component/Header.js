69191e74a7d4afaf9ded6cc893ba567557aff830
qbnyp <moneycloudy@naver.com>
2024-10-05 18:46:50 +0900

    Set : sass 설치 및 테스트 완료

--- word/src/Component/Header.js
+++ word/src/Component/Header.js
@@ -1,9 +1,11 @@
-
+import s from "../Component/Header.module.scss"
 
 export default function Header() {
     return (
         <>
             안녕
+
+            <div className={s.hi}>안녕</div>
         </>
     )
 }
